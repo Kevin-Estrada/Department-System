022f76d5c20e8ff7bf783508f8aa01ef35c5c1d4
KevinEstrada12 <43767301+KevinEstrada12@users.noreply.github.com>
2022-08-22 22:23:30 -0500

    Added a way to add departments to the in memory database

--- src/main/java/com/kevinestrada/departmentsystem/controller/DepartmentController.java
+++ src/main/java/com/kevinestrada/departmentsystem/controller/DepartmentController.java
@@ -1,8 +1,21 @@
 package com.kevinestrada.departmentsystem.controller;
 
+import com.kevinestrada.departmentsystem.entity.Department;
+import com.kevinestrada.departmentsystem.service.DepartmentService;
+import com.kevinestrada.departmentsystem.service.DepartmentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DepartmentController {
 
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/departments")
+    public Department saveDepartment(@RequestBody Department department){
+        return departmentService.saveDepartment(department);
+    }
 }
--- src/main/java/com/kevinestrada/departmentsystem/service/DepartmentService.java
+++ src/main/java/com/kevinestrada/departmentsystem/service/DepartmentService.java
@@ -1,4 +1,7 @@
 package com.kevinestrada.departmentsystem.service;
 
+import com.kevinestrada.departmentsystem.entity.Department;
+
 public interface DepartmentService {
+    Department saveDepartment(Department department);
 }
