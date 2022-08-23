4fb346b110acdab8407f791769c7cd0c231d5d7e
KevinEstrada12 <43767301+KevinEstrada12@users.noreply.github.com>
2022-08-22 23:11:28 -0500

    Added validation dependency as well as validation for Department Name

--- src/main/java/com/kevinestrada/departmentsystem/entity/Department.java
+++ src/main/java/com/kevinestrada/departmentsystem/entity/Department.java
@@ -1,15 +1,22 @@
 package com.kevinestrada.departmentsystem.entity;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long departmentId;
+    @NotBlank(message = "Please Add Department Name")
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
